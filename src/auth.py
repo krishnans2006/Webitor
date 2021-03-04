c18b414d03e1ca31ed1329b8a97f9fc52880bcda
Restyled.io <commits@restyled.io>
2021-03-04 14:44:18 +0000

    Restyled by black

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+
 # from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+
 # from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
 # @auth.route('/login', methods=["GET", "POST"])
 # def login():
