19d15900a815ee759fadc9faf1158cbfba05e273
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-03-04 10:38:25 +0000

    remove useless text

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")#where is the you dont have to put app.route check views.py in src it's cleaner and easier to have it like that
+    app.run(host="0.0.0.0")
