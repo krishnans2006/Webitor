78ddddb6958cea013f15ac885c2041b31300caa6
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-03-04 14:49:11 +0000

    added login and register endpoints (#1)
    * added login and register endpoints
    
    * Restyle added login and register endpoints (#2)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by whitespace
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,42 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint
+from flask import flash
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask_login import current_user
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+# from . import db
+# from .models import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
+
+# @auth.route('/login', methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         # email = request.form.get('email')
+#         # password = request.form.get('password') DESIGN DATABASE FIRST
+#         pass
+
+#     return render_template('login.html')#, user=current_user)
+
+# @auth.route('/logout')
+# @login_required
+# def logout():
+#     # logout_user()
+#     # return redirect(url_for('auth.login'))
+#     pass
+
+# @auth.route('/register', methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         email = request.form.get("email")
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         confirm_password = request.form.get("confirm_password")
+
+#     return render_template('register.html')#user=current_user
