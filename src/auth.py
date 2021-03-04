21185b24152953994cd799a68d8adf097630b95e
Restyled.io <commits@restyled.io>
2021-03-04 14:44:21 +0000

    Restyled by isort

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,8 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-
-# from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 # from . import db
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import current_user, login_required, login_user, logout_user
+# from .models import User
+from werkzeug.security import check_password_hash, generate_password_hash
 
 auth = Blueprint("auth", __name__)
 
