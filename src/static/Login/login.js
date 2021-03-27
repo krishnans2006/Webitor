fbb15f66064baf1507dee9c2c509af99f351e8df
Alfie Phillips <74322635+AlfiePhillips@users.noreply.github.com>
2021-03-27 20:25:30 +0000

    submitting fixes

--- src/static/Login/login.js
+++ src/static/Login/login.js
@@ -1,7 +1,19 @@
 function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/google-auth");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send("idtoken=" + id_token);
+  var id_token = googleUser.getAuthResponse().id_token;
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/google-auth");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("idtoken=" + id_token);
+}
+
+function required() {
+  let email = document.login_form.email.value;
+  let password = document.login_form.password.value;
+
+  if (email !== "" && password !== "") {
+    return true;
+  } else {
+    alert("Please input a value!");
+    return false;
+  }
 }
